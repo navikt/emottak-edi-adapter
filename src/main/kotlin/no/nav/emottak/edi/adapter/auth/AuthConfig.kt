d44b7107c351a35a01e45727610f3e8062c0e04e
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-11-18 09:49:17 +0100

    Lagt til debug logging

--- src/main/kotlin/no/nav/emottak/edi/adapter/auth/AuthConfig.kt
+++ src/main/kotlin/no/nav/emottak/edi/adapter/auth/AuthConfig.kt
@@ -4,9 +4,12 @@ import no.nav.emottak.config
 import no.nav.emottak.utils.environment.getEnvVar
 import no.nav.security.token.support.v3.IssuerConfig
 import no.nav.security.token.support.v3.TokenSupportConfig
+import org.slf4j.LoggerFactory
 
 class AuthConfig {
     companion object {
+        private val logger = LoggerFactory.getLogger(AuthConfig::class.java)
+
         fun getTokenSupportConfig(): TokenSupportConfig = TokenSupportConfig(
             IssuerConfig(
                 name = config().azureAuth.issuer.value,
@@ -15,16 +18,28 @@ class AuthConfig {
             )
         )
 
-        fun getScope(): String = getEnvVar(
-            config().azureAuth.appScope.value,
-            "api://${getEnvVar("NAIS_CLUSTER_NAME", "dev-gcp")}.team-emottak.${config().azureAuth.appName.value}/.default"
-        )
+        fun getScope(): String {
+            val scope = getEnvVar(
+                config().azureAuth.appScope.value,
+                "api://${getEnvVar("NAIS_CLUSTER_NAME", "dev-gcp")}.team-emottak.${config().azureAuth.appName.value}/.default"
+            )
+            logger.info("EDI2 test: getScope() returns: $scope")
+            return scope
+        }
 
-        fun getAzureWellKnownUrl(): String = getEnvVar(
-            "AZURE_APP_WELL_KNOWN_URL",
-            "http://localhost:3344/${getEnvVar("AZURE_APP_TENANT_ID", config().azureAuth.issuer.value)}/.well-known/openid-configuration"
-        )
+        fun getAzureWellKnownUrl(): String {
+            val url = getEnvVar(
+                "AZURE_APP_WELL_KNOWN_URL",
+                "http://localhost:3344/${getEnvVar("AZURE_APP_TENANT_ID", config().azureAuth.issuer.value)}/.well-known/openid-configuration"
+            )
+            logger.info("EDI2 test: getAzureWellKnownUrl() returns: $url")
+            return url
+        }
 
-        private fun getAcceptedAudience(): List<String> = listOf(getEnvVar("AZURE_APP_CLIENT_ID", getScope()))
+        private fun getAcceptedAudience(): List<String> {
+            val audience = listOf(getEnvVar("AZURE_APP_CLIENT_ID", getScope()))
+            logger.info("EDI2 test: getAcceptedAudience() returns: $audience")
+            return audience
+        }
     }
 }
