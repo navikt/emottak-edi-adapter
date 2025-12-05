643087a3fcfab1c224f245e0ad2363106667981f
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2025-12-05 13:40:24 +0100

    Cleaned up scoped client

--- edi-adapter-client/src/main/kotlin/no/nav/emottak/ediadapter/client/Utils.kt
+++
@@ -1,4 +0,0 @@
-package no.nav.emottak.ediadapter.client
-
-fun getEnvVar(varName: String, defaultValue: String? = null) =
-    System.getProperty(varName) ?: System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Environment: Missing required variable \"$varName\"")
